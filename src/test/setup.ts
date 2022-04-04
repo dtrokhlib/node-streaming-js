4f6886cd267daa3b56099ff2a57d41506ad4c8ff
Dmitry Trokhlib <fgh98zip@gmail.com>
2022-04-04 13:40:51 +0300

    test

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { Application } from '../app';
-import { Express } from 'express';
 
 let mongo: any;
 
